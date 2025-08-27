23ab80d3aac1cf3a9ef85489c69f1cfbe9ffc660
Erickson45 <115362666+Erickson45@users.noreply.github.com>
2025-08-27 16:26:59 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,26 @@
+// Instalação do Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
+    // Cache de todos os arquivos essenciais para uso offline
     caches.open('minha-feira-v1').then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        'android-icon-192x192.png',
-        '3132693.png',
+        './',
+        './index.html',
+        './manifest.json',
+        './android-icon-192x192.png',
+        './3132693.png',
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
         'https://cdn.jsdelivr.net/npm/chart.js'
       ]);
+    })
+  );
+});
+
+// Responde com o conteúdo do cache se offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
